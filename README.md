c5d29272a432acad9157afdb082f321acba05628
yuki-shi <88805836+yuki-shi@users.noreply.github.com>
2023-05-15 18:53:46 -0300

    added readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# gupyuki: i don't wanna be a NEET anymore
+simple CLI script for Gupy-related job-hunting.
+returns a table with the last 10 job listings for the inputted keyword.
+
+## usage
+after creating an env on the same directory and installing the requirements, run
+
+```bash
+$ chmod u+x main.py
+$ ./main.py -k JOB_NAME
+```
+where JOB_NAME is the keyword to be searched on Gupy.
--- main.py
+++ main.py
@@ -6,27 +6,30 @@ from tabulate import tabulate
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser(description='')
-parser.add_argument('--keyword',
-                    '-k',
-                    type=str,
-                    required=True)
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--keyword',
+                        '-k',
+                        type=str,
+                        required=True)
+    args = parser.parse_args()
 
-keyword = args.keyword
-response = ((requests.get(f'https://portal.gupy.io/job-search/term={keyword}')
-                     .text))
-soup = BeautifulSoup(response, 'html.parser')
+    keyword = args.keyword
+    response = ((requests.get(f'https://portal.gupy.io/job-search/term={keyword}')
+                         .text))
+    soup = BeautifulSoup(response, 'html.parser')
 
-vagas = {}
-list_items = soup.find_all('li')
+    vagas = {}
+    list_items = soup.find_all('li')
 
-for item in list_items:
-    vagas[item.find('h4').text] = item.find_all('p')[-1].text
+    for item in list_items:
+        vagas[item.find('h4').text] = item.find_all('p')[-1].text
 
-df = (pd.DataFrame([vagas]).transpose()
-                           .reset_index(names='vaga')
-                           .rename(columns={0: 'data'}))
-df['data'] = df['data'].str.replace(r'.*(?<= )', '', regex=True)
+    df = (pd.DataFrame([vagas]).transpose()
+                               .reset_index(names='vaga')
+                               .rename(columns={0: 'data'}))
+    df['data'] = df['data'].str.replace(r'.*(?<= )', '', regex=True)
 
-print(tabulate(df, tablefmt='psql'))
+    print(tabulate(df,
+                   headers=['vaga', 'data de publicação'],
+                   tablefmt='psql'))
